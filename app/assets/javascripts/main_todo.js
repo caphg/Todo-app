a54e7cfe919076d92f17e49cd19b798e0cf466c0
Hrvoje Grdic <hrvoje.grdic@ericsson.com>
2014-08-08 15:37:19 +0200

    angular

--- app/assets/javascripts/main_todo.js
+++ app/assets/javascripts/main_todo.js
@@ -1,16 +1,41 @@
-angular.module("todoApp", [])
-    .controller("TodoController", function($scope, $http) {
-        $scope.myData = {};
+var app = angular.module('todoApp', ['ngResource']);
 
-        var resp = $http.get("/items.json");
-        resp.success(function(data, status, headers, config) {
-            alert(data);
-            $scope.myData.items = data;
+app.controller("TodoController", function($scope, $http, Item) {
+
+        $scope.tasks = {};
+
+        $scope.editNameEnabled = false;
+
+
+        $scope.addTask = function(name, description, priority, done, due_date){
+            $scope.tasks.push({name: name, description: description, priority: priority, done: done, due_date: due_date});
+        };
+
+        $scope.editName = function(){
+            this.editNameEnabled = true;
+        };
+
+        $scope.saveName = function(id, index, name){
+            var item = Item.get({ id: id }, function() {
+                item.name = name;
+                Item.update({ id:id }, item);
+
+                $scope.tasks[index].name = name;
+                $scope.editNameEnabled = false;
+            });
+
+        };
+
+
+        Item.query(function(data) {
+            $scope.tasks = data;
         });
 
-        resp.error(function(data, status, headers, config) {
-            alert("AJAX failed!");
-        });
 
     });
 
+app.factory("Item", function($resource) {
+    return $resource("/items/:id.json", null, {
+        'update': { method: 'PUT' }
+    });
+});
